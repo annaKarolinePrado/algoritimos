5a2df2dbda9f9c80c9b0d0c6d276b4f1c7db5f2d
Anna Prado <annakaroline9298@gmail.br>
2023-10-31 22:25:43 -0300

    Criado projeto para estudo de lógica de programação.

---
+++ src/pagina_18/Exercicio01.java
@@ -0,0 +1,14 @@
+package pagina_18;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+
+        int idade = 20;
+
+        if (idade >= 18){
+            System.out.println("Você já atingiu a maioridade");
+        }else{
+            System.out.println("Você ainda não atingiu a maioridade");
+        }
+    }
+}
