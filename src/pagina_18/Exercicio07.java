8f54bd5da2c072d1d3c3d3167151e23616780e8a
Anna Prado <annakaroline9298@gmail.br>
2023-11-02 19:45:58 -0300

    Criado algorítimo para calcular uma classificação de acordo com a idade informada.

---
+++ src/pagina_18/Exercicio07.java
@@ -0,0 +1,22 @@
+package pagina_18;
+import com.sun.source.tree.IfTree;
+public class Exercicio07 {
+    public static void main(String[] args) {
+        String MSG_INFANTIL = "%s com %s anos se enquadra na categoria INFANTIL.";
+        String MSG_ADULTO   = "%s com %s anos se enquadra na categoria ADULTO.";
+        String MSG_JUVENIL  = "%s com %s anos se enquadra na categoria JUVENIL.";
+
+        String nome = "Rebecca";
+        int idade = 100;
+
+        if ((idade >= 6) && (idade <= 10)){
+            System.out.println(String.format(MSG_INFANTIL,nome,idade));
+        } else if ((idade <= 17)) {
+            System.out.println(String.format(MSG_JUVENIL,nome,idade));
+        } else if (idade >= 18) {
+            System.out.println(String.format(MSG_ADULTO,nome,idade));
+        } else {
+            System.out.println("Idade não classificada.");
+        }
+    }
+}
