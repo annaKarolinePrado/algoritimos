233de2c6f4d80d58d054e6d89a3399baa7938de1
Anna Prado <annakaroline9298@gmail.br>
2023-11-02 15:42:22 -0300

    Criado algorítimo para calcular a média de três notas e mostrar se o aluo foi aprovado ou reprovado.

---
+++ src/pagina_18/Exercicio05.java
@@ -0,0 +1,20 @@
+package pagina_18;
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        String MSG_APROVADO = "O aluno %s obteve as notas %s, %s e %s com média %s foi APROVADO.";
+        String MSG_REPROVADO = "O aluno %s obteve as notas %s, %s e %s com média %s foi REPROVADO.";
+
+        String nome = "Ambrósio";
+        float nota01 = 10;
+        float nota02 = 1;
+        float nota03 = 1;
+        float media = (nota01 + nota02 + nota03) / 3;
+
+        if (media >= 7){
+            System.out.println(String.format(MSG_APROVADO,nome,nota01,nota02,nota03,media));
+        }else {
+            System.out.println(String.format(MSG_REPROVADO,nome,nota01,nota02,nota03,media));
+        }
+    }
+}
