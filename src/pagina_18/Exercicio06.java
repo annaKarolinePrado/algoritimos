b820984b449a222349abd12b4c4bbcda2b78b229
Anna Prado <annakaroline9298@gmail.br>
2023-11-02 19:07:31 -0300

    Criado algorítimo para calcular o maior entre dois números.

---
+++ src/pagina_18/Exercicio06.java
@@ -0,0 +1,20 @@
+package pagina_18;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+        String MSG_NUM_MAIOR = "Entre os números %s e %s, o MAIOR é o número %s.";
+
+        Float numero01 = 15.5F;
+        Float numero02 = 15.5F;
+
+        if (!numero01.equals(numero02)){
+            if(numero01 > numero02){
+                System.out.println(String.format(MSG_NUM_MAIOR,numero01,numero02,numero01));
+            }else {
+                System.out.println(String.format(MSG_NUM_MAIOR,numero01,numero02,numero02));
+            }
+        }else {
+            System.out.println("Os números digitados são iguais. Favor inserir números diferentes.");
+        }
+    }
+}
